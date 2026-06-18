46972e21ce410516b9c546e6c1960a2c35684adc
Александр Гребенников <kvartirka199008@gmail.com>
2026-06-18 13:33:40 +0700

    new prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,27 +1,23 @@
 #C:\Users\Developer\PycharmProjects\devrange\config\settings\prod.py
+from .base import *
 import os
 
 DEBUG = False
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 
-CORS_ALLOWED_ORIGINS = [
-    FRONTEND_URL
-] if FRONTEND_URL else []
+CORS_ALLOWED_ORIGINS = [FRONTEND_URL] if FRONTEND_URL else []
 
-CSRF_TRUSTED_ORIGINS = [
-    FRONTEND_URL
-] if FRONTEND_URL else []
+CSRF_TRUSTED_ORIGINS = [FRONTEND_URL] if FRONTEND_URL else []
 
 SECURE_PROXY_SSL_HEADER = (
     "HTTP_X_FORWARDED_PROTO",
     "https"
 )
 
-# 🔥 ВАЖНО ДЛЯ HTTP (без домена и HTTPS)
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
